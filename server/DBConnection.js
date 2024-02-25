296d762a52e3d3285c94d95411107382852e6daa
JD93Lopez <juandavid.jdlea@gmail.com>
2024-02-24 19:24:55 -0500

    archivos configs

--- server/DBConnection.js
+++ server/DBConnection.js
@@ -1,17 +1,13 @@
 const {Pool} = require ('pg');
+const FileReader = require ('./FileReader.js');
 
-const config = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'rhinodb',
-    password: 'password',
-    port: 5432,
-};
+const config = FileReader.readDBConfig()
 const pool = new Pool(config);
 const obtenerUsuarios = async () => {
-  const res = await pool.query('select * from usuarios')
-  console.log(res.rows);
+  const DBRes = await pool.query('select * from usuarios')
+  // console.log(DBRes.rows);
   pool.end();
+  return DBRes
 }
 const insertarUsuario = async (idusuarios, nombre_usuario, contrasena, nombre_real, direccion, telefono, identificacion, correo, tipo_identificacion, tipo_usuario) => {
   const texto = 'insert into usuarios(idusuarios, nombre_usuario, contrasena, nombre_real, direccion, telefono, identificacion, correo, tipo_identificacion, tipo_usuario) values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)'
@@ -19,12 +15,13 @@ const insertarUsuario = async (idusuarios, nombre_usuario, contrasena, nombre_re
   const values = [idusuarios, nombre_usuario, contrasena, nombre_real, direccion, telefono, identificacion, correo, tipo_identificacion, tipo_usuario]
   // [1,"juan", "1234", "Juan D Lopez", "Calle # Carrera", "300", "109", "juandavid.jdlea@gmail.com", "CEDULA", "ADMINISTRADOR"]
 
-  const consulta = await pool.query(texto, values);
-  console.log(consulta);
+  const DBRes = await pool.query(texto, values);
+  // console.log(DBRes);
   pool.end();
+  return DBRes
 }
 
-module.exports = {obtenerUsuarios};
+module.exports = {obtenerUsuarios, insertarUsuario};
 
 // insertarUsuario();
 /* 
--- server/server.js
+++ server/server.js
@@ -1,30 +1,55 @@
 const express = require('express');
 const cors = require('cors');
-const connection = require('./connection.js');
+const FileReader = require('./FileReader.js');
+const DBConnection = require('./DBConnection.js');
 
-const app = express();
-const PORT = 3001; // Puerto del servidor
+const config = FileReader.readServerConfig()
+
+const server = express();
+const PORT = config.port; // Puerto del servidor
 
 //Puertos e ips permitidas
-app.use(cors({ origin: 'http://localhost:3000' }));
+const corsLinks = config.corsLinks
+corsLinks.forEach(link => {
+    server.use(cors({ origin: link }));
+});
 
-// Rutas
-app.get('/', (req, res) => {
+// Rutas prueba
+server.get('/', (req, res) => {
     res.send('¡Hola, mundo!');
 });
-
-app.get('/api/ejemplo', (req, res) => {
+server.get('/api/ejemplo', (req, res) => {
     res.json({ mensaje: 'Esta es una API de ejemplo'});
 });
-
-app.get('/api/saludo/:nombre/:apellido', (req, res) => {
+server.get('/api/saludo/:nombre/:apellido', (req, res) => {
     const nombre = req.params.nombre;
     const apellido = req.params.apellido;
     res.json({ mensaje: `¡Hola, ${nombre} ${apellido}! Bienvenido a nuestra API.` });
 });
 
+//Obtener usuarios
+server.get('/api/get/usuarios/:usuario/:contrasena', async (req, res) => {
+    const usuario = req.params.usuario;
+    const contrasena = req.params.contrasena;
+    //TODO comprobar usuario y contrasena
+    res.json({ DBRes: await DBConnection.obtenerUsuarios() });
+});
+
+//Insertar usuario
+server.get('/api/insert/usuarios/:JSONUsuario/:usuario/:contrasena', async (req, res) => {
+    const usuario = req.params.usuario;
+    const contrasena = req.params.contrasena;
+    //TODO comprobar usuario y contrasena
+    const JSONUsuario = req.params.JSONUsuario;
+    const usuarioInsertar = JSON.parse(JSONUsuario)
+    DBConnection.insertarUsuario(usuarioInsertar.idusuarios, usuarioInsertar.nombre_usuario, usuarioInsertar.contrasena, usuarioInsertar.nombre_real, usuarioInsertar.direccion, usuarioInsertar.telefono, usuarioInsertar.identificacion, usuarioInsertar.correo, usuarioInsertar.tipo_identificacion, usuarioInsertar.tipo_usuario)
+    res.json({ DBRes: DBConnection.obtenerUsuarios() });
+});
+
 // Iniciar
-app.listen(PORT, () => {
-    connection.obtenerUsuarios()
+server.listen(PORT, () => {
+    // DBConnection.obtenerUsuarios().then((result) => {
+    //     console.log(result.rows)
+    // })
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
