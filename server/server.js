09b554a4ccc3d973aea9d1f1e42f0ec9eb520f7d
JD93Lopez <juandavid.jdlea@gmail.com>
2024-02-26 20:44:20 -0500

    TryCatch Api

--- server/server.js
+++ server/server.js
@@ -29,65 +29,89 @@ server.get('/api/saludo/:nombre/:apellido', (req, res) => {
 
 //Obtener usuarios
 server.get('/api/get/usuarios/:usuario/:contrasena', async (req, res) => {
-    const usuario = req.params.usuario;
-    const contrasena = req.params.contrasena;
-    //TODO comprobar usuario y contrasena
-    res.json({ DBRes: await DBConnection.obtenerUsuarios() });
+    try {
+        const usuario = req.params.usuario;
+        const contrasena = req.params.contrasena;
+        //TODO comprobar usuario y contrasena
+        res.json({ DBRes: await DBConnection.obtenerUsuarios() });
+    } catch (error) {
+        res.json({ DBRes: error });
+    }
 });
 
 //Insertar usuario
 server.get('/api/insert/usuarios/:JSONUsuario/:usuario/:contrasena', async (req, res) => {
-    const usuario = req.params.usuario;
-    const contrasena = req.params.contrasena;
-    //TODO comprobar usuario y contrasena
-    const JSONUsuario = req.params.JSONUsuario;
-    const usuarioInsertar = JSON.parse(JSONUsuario)
-    DBConnection.insertarUsuario(usuarioInsertar.idusuarios, usuarioInsertar.nombre_usuario, usuarioInsertar.contrasena, usuarioInsertar.nombre_real, usuarioInsertar.direccion, usuarioInsertar.telefono, usuarioInsertar.identificacion, usuarioInsertar.correo, usuarioInsertar.tipo_identificacion, usuarioInsertar.tipo_usuario)
-    res.json({ DBRes: "Insercion Finalizada" });
+    try {
+        const usuario = req.params.usuario;
+        const contrasena = req.params.contrasena;
+        //TODO comprobar usuario y contrasena
+        const JSONUsuario = req.params.JSONUsuario;
+        const usuarioInsertar = JSON.parse(JSONUsuario)
+        DBConnection.insertarUsuario(usuarioInsertar.idusuarios, usuarioInsertar.nombre_usuario, usuarioInsertar.contrasena, usuarioInsertar.nombre_real, usuarioInsertar.direccion, usuarioInsertar.telefono, usuarioInsertar.identificacion, usuarioInsertar.correo, usuarioInsertar.tipo_identificacion, usuarioInsertar.tipo_usuario)
+        res.json({ DBRes: "Insercion Finalizada" });
+    } catch (error) {
+        res.json({ DBRes: error });
+    }
 });
 
 //Insertar proveedor
 server.get('/api/insert/proveedores/:JSONProveedor/:usuario/:contrasena', async (req, res) => {
-    const usuario = req.params.usuario;
-    const contrasena = req.params.contrasena;
-    //TODO comprobar usuario y contrasena
-    const JSONProveedor = req.params.JSONProveedor;
-    const proveedorInsertar = JSON.parse(JSONProveedor)
-    DBConnection.insertarProveedor(proveedorInsertar.nombre, proveedorInsertar.nit, proveedorInsertar.direccion, proveedorInsertar.descripcion, proveedorInsertar.telefono)
-    res.json({ DBRes: "Insercion Finalizada" });
+    try {
+        const usuario = req.params.usuario;
+        const contrasena = req.params.contrasena;
+        //TODO comprobar usuario y contrasena
+        const JSONProveedor = req.params.JSONProveedor;
+        const proveedorInsertar = JSON.parse(JSONProveedor)
+        DBConnection.insertarProveedor(proveedorInsertar.nombre, proveedorInsertar.nit, proveedorInsertar.direccion, proveedorInsertar.descripcion, proveedorInsertar.telefono)
+        res.json({ DBRes: "Insercion Finalizada" });
+    } catch (error) {
+        res.json({ DBRes: error });
+    }
 });
 
 //Insertar producto
 server.get('/api/insert/productos/:JSONObject/:usuario/:contrasena', async (req, res) => {
-    const usuario = req.params.usuario;
-    const contrasena = req.params.contrasena;
-    //TODO comprobar usuario y contrasena
-    const JSONObject = req.params.JSONObject;
-    const objetoAInsertar = JSON.parse(JSONObject)
-    DBConnection.insertarProducto(objetoAInsertar.nombre, objetoAInsertar.descripcion, objetoAInsertar.identificacion, objetoAInsertar.precio_alquiler, objetoAInsertar.precio_compra, objetoAInsertar.marca, objetoAInsertar.modelo, objetoAInsertar.tipo_vehiculo)
-    res.json({ DBRes: "Insercion Finalizada" });
+    try {
+        const usuario = req.params.usuario;
+        const contrasena = req.params.contrasena;
+        //TODO comprobar usuario y contrasena
+        const JSONObject = req.params.JSONObject;
+        const objetoAInsertar = JSON.parse(JSONObject)
+        DBConnection.insertarProducto(objetoAInsertar.nombre, objetoAInsertar.descripcion, objetoAInsertar.identificacion, objetoAInsertar.precio_alquiler, objetoAInsertar.precio_compra, objetoAInsertar.marca, objetoAInsertar.modelo, objetoAInsertar.tipo_vehiculo)
+        res.json({ DBRes: "Insercion Finalizada" });
+    } catch (error) {
+        res.json({ DBRes: error });
+    }
 });
 
 //Insertar proyecto
 server.get('/api/insert/proyectos/:JSONObject/:usuario/:contrasena', async (req, res) => {
-    const usuario = req.params.usuario;
-    const contrasena = req.params.contrasena;
-    //TODO comprobar usuario y contrasena
-    const JSONObject = req.params.JSONObject;
-    const objetoAInsertar = JSON.parse(JSONObject)
-    DBConnection.insertarProyecto(objetoAInsertar.nombre, objetoAInsertar.identificacion, objetoAInsertar.tipo_identificacion, objetoAInsertar.telefono, objetoAInsertar.descripcion, objetoAInsertar.fecha_entrega, objetoAInsertar.estado_avance)
-    res.json({ DBRes: "Insercion Finalizada" });
+    try {
+        const usuario = req.params.usuario;
+        const contrasena = req.params.contrasena;
+        //TODO comprobar usuario y contrasena
+        const JSONObject = req.params.JSONObject;
+        const objetoAInsertar = JSON.parse(JSONObject)
+        DBConnection.insertarProyecto(objetoAInsertar.nombre, objetoAInsertar.identificacion, objetoAInsertar.tipo_identificacion, objetoAInsertar.telefono, objetoAInsertar.descripcion, objetoAInsertar.fecha_entrega, objetoAInsertar.estado_avance)
+        res.json({ DBRes: "Insercion Finalizada" });
+    } catch (error) {
+        res.json({ DBRes: error });
+    }
 });
 
 //Insertar conductor
 server.get('/api/insert/conductores/:JSONObject/:usuario/:contrasena', async (req, res) => {
-    const usuario = req.params.usuario;
-    const contrasena = req.params.contrasena;
-    //TODO comprobar usuario y contrasena
-    const JSONObject = req.params.JSONObject;
-    const objetoAInsertar = JSON.parse(JSONObject)
-    DBConnection.insertarConductor(objetoAInsertar.nombre, objetoAInsertar.cedula, objetoAInsertar.telefono)
-    res.json({ DBRes: "Insercion Finalizada" });
+    try {
+        const usuario = req.params.usuario;
+        const contrasena = req.params.contrasena;
+        //TODO comprobar usuario y contrasena
+        const JSONObject = req.params.JSONObject;
+        const objetoAInsertar = JSON.parse(JSONObject)
+        DBConnection.insertarConductor(objetoAInsertar.nombre, objetoAInsertar.cedula, objetoAInsertar.telefono)
+        res.json({ DBRes: "Insercion Finalizada" });
+    } catch (error) {
+        res.json({ DBRes: error });
+    }
 });
 
 // Iniciar
