a9d58a4a2a71a4c07413713d76b6bcf1003cae58
darwing26 <darwing.afanador26@gmail.com>
2024-03-29 23:06:02 -0500

    botones y estilos de la barra de busqueda en la vista administrador Mantenimientos

--- vista-admin-v1/src/pages/VistaAdministradorMantenim.js
+++ vista-admin-v1/src/pages/VistaAdministradorMantenim.js
@@ -1,36 +1,81 @@
-import { useContext } from "react";
-import { DataContext } from "../components/DataProvider";
+import React, { useState, useContext, useEffect, useCallback } from 'react';
 import FrameComponent1 from "../components/FrameComponent1";
-import TarjetaMantenimiento from "../components/TarjetaMantenimiento";
 import styles from "./VistaAdministradorMantenim.module.css";
+import { DataContext } from "../components/DataProvider";
+import TarjetaMantenimiento from "../components/TarjetaMantenimiento";
+import orden from "../OrdenamientoSimilitud";
+
+export const SearchContext = React.createContext();
 
 const VistaAdministradorMantenim = () => {
-
   const dataContext = useContext(DataContext);
+  const { Loaded, mantenimientos: mantenimientosContext, usuarios } = dataContext;
+  const [busqueda, setBusqueda] = useState('');
+  const [mantenimientos, setMantenimientos] = useState([]);
+
+  useEffect(() => {
+    if (Loaded) {
+      setMantenimientos(mantenimientosContext);
+    }
+  }, [Loaded, mantenimientosContext]);
+
+  const buscar = useCallback(() => {
+    if (!mantenimientosContext) {
+      return;
+    }
   
-  if (!dataContext.Loaded) {
+    let nuevosMantenimientos = mantenimientosContext.map((mantenimiento) => {
+      const similitud = orden.calcularSimilitud(busqueda, mantenimiento.identificacion);
+      return { mantenimiento: mantenimiento, similitud: similitud };
+    }).sort((a, b) => b.similitud - a.similitud);
+    nuevosMantenimientos = nuevosMantenimientos.map((mantenimientoSimilitud)=>{
+      return mantenimientoSimilitud.mantenimiento
+    })
+    
+  
+    setMantenimientos(nuevosMantenimientos);
+  }, [mantenimientosContext, busqueda]);
+
+  useEffect(() => {
+    buscar();
+  }, [buscar, busqueda]);
+
+  if (!Loaded) {
     return <div>Cargando... Por favor espere.</div>;
   }
-
-  const mantenimientos = dataContext.mantenimientos
-
   return (
-    <div className={styles.vistaAdministradorMantenim}>
-      <div className={styles.vistaAdministradorMantenimChild} />
-      <FrameComponent1 />
-      <main className={styles.vistaAdministradorMantenimInner}>
-        <section className={styles.mantenimientoParent}>
-          <h1 className={styles.mantenimiento}>MANTENIMIENTO</h1>
-          <div className={styles.projectDetails}>
-            {mantenimientos.map(mantenimiento => (
-              <TarjetaMantenimiento
-                nombreMaquinaria={mantenimiento.nombreMaquinaria}
-                identificacion={mantenimiento.identificacion}
-                fecha={mantenimiento.fecha}
-                key={mantenimiento.identificacion}
-              />
-            ))}
-            <div className={styles.actualizarUsuarioParent}>
+    <DataContext.Provider value={{ Loaded, mantenimientos: mantenimientosContext, usuarios }}>
+      <SearchContext.Provider value={{ busqueda, setBusqueda }}>
+        <div className={styles.vistaAdministradorMantenim}>
+          <div className={styles.vistaAdministradorMantenimChild} />
+          <FrameComponent1 />
+          <main className={styles.vistaAdministradorMantenimInner}>
+            <section className={styles.mantenimientoParent}>
+              <h1 className={styles.mantenimiento}>MANTENIMIENTO</h1>
+            <div className={styles.buscarUsuario}>
+              <div className={styles.buscarUsuarioChild} />
+              <img className ={styles.lupa4Icon} alt="" src="/lupa-4@2x.png"/>
+            <input
+              className={styles.buscarMantenimiento}
+              placeholder="buscar mantenimiento"
+              type="text"
+              onChange={(event) => setBusqueda(event.target.value)}
+              id = "inputbuscarmantenimiento"
+              useref= "inputbuscarmantenimiento"
+            />
+            </div>
+              <div className={styles.projectDetails}>
+                {mantenimientos.map(mantenimiento => {
+                  return <TarjetaMantenimiento
+                    object = {mantenimiento}
+                    nombreMaquinaria = {mantenimiento.nombreMaquinaria}
+                    identificacion = {mantenimiento.identificacion}
+                    fecha = {mantenimiento.fecha}
+                    key = {mantenimiento.identificacion}
+                  ></TarjetaMantenimiento>
+                })}
+              </div>
+              <div className={styles.actualizarUsuarioParent}>
               <button
                 className={styles.actualizarUsuario}
               >
@@ -51,10 +96,11 @@ const VistaAdministradorMantenim = () => {
                 <div className={styles.EliminarSeleccionados} >Eliminar</div>
               </button>
             </div>
-          </div>
-        </section>
-      </main>
-    </div>
+            </section>
+          </main>
+        </div>
+      </SearchContext.Provider>
+    </DataContext.Provider>
   );
 };
 
