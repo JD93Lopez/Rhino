68e2acd01a06cc4ba9e52fbd21fe68ab89a11599
JD93Lopez <juandavid.jdlea@gmail.com>
2024-04-06 19:46:11 -0500

    Inicio de sesion.

--- vista-admin-v1/src/pages/VistaAdministradorLogin.js
+++ vista-admin-v1/src/pages/VistaAdministradorLogin.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './VistaAdministradorLogin.css'; 
 import { useNavigate } from 'react-router-dom';
+import { DataContext } from "../components/DataProvider";
 
 const VistaAdministradorLogin = () => {
-const navigate = useNavigate();
+  
+  const dataContext = useContext(DataContext)
+
+  if(!dataContext.Loaded){
+    return <p>Cargando...</p>
+  }
+
+  const navigate = useNavigate();
   const handleSubmit = (event) => {
     event.preventDefault();
     // Aquí puedes agregar la lógica para manejar el envío del formulario
-    console.log("Formulario enviado");
+    dataContext.usuarios.usuarioIniciado.nombre_usuario = document.getElementById("campoNombre").value
+    dataContext.usuarios.usuarioIniciado.contrasena = document.getElementById("campoConstrasena").value
+    //TODO iniciar sesion AXIOS
+
     navigate('/vista-administrador-usuarios'); 
   };
 
@@ -17,10 +28,10 @@ const navigate = useNavigate();
       <p id="heading">Inicio de Sesion Administrador</p>
       <img src="/logofinalremovebgpreview-1@2x.png" alt="Logo" className="logo" />
       <div className="field">
-        <input autoComplete="off" placeholder="Usuario" className="input-field" type="text" />
+        <input autoComplete="off" placeholder="Usuario" className="input-field" type="text" id='campoNombre' useref='campoNombre'/>
       </div>
       <div className="field">
-        <input placeholder="Contraseña" className="input-field" type="password" />
+        <input placeholder="Contraseña" className="input-field" type="password" id='campoContrasena' useref='campoContrasena' />
       </div>
       <div className="btn">
         <button type="submit" className="button1">Iniciar Sesion</button>
