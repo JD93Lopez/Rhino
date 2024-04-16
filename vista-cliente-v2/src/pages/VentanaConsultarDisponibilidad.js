b4cfb92970a0d1ae59e24ce57bf4f982420cab09
SebasPico24 <picoafanador@gmail.com>
2024-04-16 09:24:17 -0500

    se arreglaron los input en la ventana de disponibilidad

--- vista-cliente-v2/src/pages/VentanaConsultarDisponibilidad.js
+++ vista-cliente-v2/src/pages/VentanaConsultarDisponibilidad.js
@@ -1,8 +1,8 @@
 import FrameComponent3 from "../components/FrameComponent11";
-import GroupComponent1 from "../components/GroupComponent9";
-import GroupComponent from "../components/GroupComponent8";
+import GroupComponent1 from "../components/GroupComponent2";
+import GroupComponent from "../components/GroupComponent3";
 import FrameComponent2 from "../components/FrameComponent10";
-import styles from "./VentanaConsultarDisponibilid.module.css";
+import styles from "./VentanaConsultarDisponibilidad.module.css";
 
 const VentanaConsultarDisponibilid = () => {
   return (
@@ -10,8 +10,8 @@ const VentanaConsultarDisponibilid = () => {
       <FrameComponent11 />
       <section className={styles.ventanaConsultarDisponibilidInner}>
         <div className={styles.frameParent}>
-          <GroupComponent9 />
-          <GroupComponent8 />
+          <GroupComponent3 />
+          <GroupComponent2 />
           <b className={styles.LugarOrigen}>Lugar de Origen</b>
           <input type="text" placeholder="Input 1" />
           <b className={styles.LugarDestino}>Lugar de DEstino</b>
