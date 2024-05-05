91271dea1936423fc746ef3acc20f5fab6166c25
JD93Lopez <juandavid.jdlea@gmail.com>
2024-05-05 01:52:25 -0500

    Informe de activos.

--- vista-admin-v1/src/pages/VistaAdministradorInformes.js
+++ vista-admin-v1/src/pages/VistaAdministradorInformes.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styles from "./VistaAdministradorInformes.module.css";
+import axios from '../axios';
 
 // Definimos el componente ProductCard
 const ProductCard = ({ product }) => {
@@ -7,45 +8,45 @@ const ProductCard = ({ product }) => {
     <div className={styles.productCard}>
       <h2>Informe de Producto</h2>
       <div className={styles.productImage}>
-        <img src={product.imageUrl} alt={product.name} />
+        <img src={product.imagen} alt={product.name} />
       </div>
       <table className={styles.productTable}>
         <tbody>
           <tr>
             <th>Nombre del Producto:</th>
-            <td>{product.name}</td>
+            <td>{product.nombre}</td>
           </tr>
           <tr>
             <th>ID del Producto:</th>
-            <td>{product.id}</td>
+            <td>{product.idproductos}</td>
           </tr>
           <tr>
             <th>Descripción:</th>
-            <td>{product.description}</td>
+            <td>{product.descripcion}</td>
           </tr>
           <tr>
             <th>Precio de Alquiler:</th>
-            <td>${product.price}</td>
+            <td>${product.precio_alquiler}</td>
           </tr>
           <tr>
             <th>Modelo:</th>
-            <td>{product.model || 'N/A'}</td>
+            <td>{product.modelo || 'N/A'}</td>
           </tr>
           <tr>
             <th>Fabricante:</th>
-            <td>{product.manufacturer || 'N/A'}</td>
+            <td>{product.marca || 'N/A'}</td>
           </tr>
           <tr>
             <th>Tipo:</th>
-            <td>{product.type || 'N/A'}</td>
+            <td>{product.tipo_vehiculo || 'N/A'}</td>
           </tr>
           <tr>
             <th>Estado:</th>
-            <td>{product.status || 'N/A'}</td>
+            <td>{product.estado || 'N/A'}</td>
           </tr>
           <tr>
-            <th>Fecha:</th>
-            <td>{product.rentalDate || 'N/A'}</td>
+            <th>Fecha agregado:</th>
+            <td>{product.fecha&&product.fecha.substring(0,10) || 'N/A'}</td>
           </tr>
         </tbody>
       </table>
@@ -53,19 +54,27 @@ const ProductCard = ({ product }) => {
   );
   };
 
-  const VistaAdministradorInformes = () => {
+const VistaAdministradorInformes = () => {
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
 
-  const products = [
-    { id: 1, name: 'Producto 1', description: 'Descripción del producto 1', price: 100, model: 'Modelo 1', manufacturer: 'Fabricante 1', type: 'Transporte', status: 'Disponible', imageUrl: 'https://via.placeholder.com/300x200', rentalDate: '2023-05-01' },
-    { id: 2, name: 'Producto 2', description: 'Descripción del producto 2', price: 200, model: 'Modelo 2', manufacturer: 'Fabricante 2', type: 'Maquinaria Pesada', status: 'Ocupado', imageUrl: 'https://via.placeholder.com/300x200', rentalDate: '2023-04-15' },
-    { id: 3, name: 'Producto 3', description: 'Descripción del producto 3', price: 300, model: 'Modelo 3', manufacturer: 'Fabricante 3', type: 'Transporte', status: 'Fuera de Servicio', imageUrl: 'https://via.placeholder.com/300x200', rentalDate: '2023-03-01' },
-  ];
+  const [products,setProducts] = useState()
+
+  if(!products){
+    axios.api(`obtener/productos`).then((res)=>{
+      try {
+        setProducts(res.data.Res)
+      } catch (e) {}
+    })
+  }
+
+  if(!products){
+    return
+  }
 
   // Filtro de productos según fechas (Asumiendo que cada producto tiene un campo 'rentalDate')
   const filteredProducts = products.filter((product) => {
-    const rentalDate = new Date(product.rentalDate);
+    const rentalDate = new Date(product.fecha);
     const startDateObj = startDate ? new Date(startDate) : null;
     const endDateObj = endDate ? new Date(endDate) : null;
 
@@ -92,7 +101,7 @@ const ProductCard = ({ product }) => {
       </div>
       <div>
         {filteredProducts.map((product) => (
-          <ProductCard key={product.id} product={product} />
+          <ProductCard key={product.idproductos} product={product} />
         ))}
       </div>
     </div>
